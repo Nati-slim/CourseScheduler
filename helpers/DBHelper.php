4b1053fada9cb1c7a3f940c9a7275a137cea8d75
Jane Ullah <janeullah@gmail.com>
2013-04-30 15:42:06 -0400

    Fixing database helper issues.

--- helpers/DBHelper.php
+++ helpers/DBHelper.php
@@ -6,7 +6,9 @@ require_once("Section.php");
 require_once("Meeting.php");
 class DBHelper{
 	private $listcourses;
+	private $singlecourse;
 	private $listsections;
+	private $singlesection;
 	private $errorMessage;
 
 	//Default constructor
@@ -17,32 +19,91 @@ class DBHelper{
 				echo "Failed to connect to MySQL: (" . $this->dbconn->connect_errno . ") " . $this->dbconn->error;
 			}else{
 				//echo $this->dbconn->host_info . "\n";
-				$this->listcourses = $this->dbconn->prepare("SELECT * from Requirements where requirementId = ? and coursePrefix = ? and courseNumber = ?");
+				$this->singlecourse = $this->dbconn->prepare("SELECT * from Requirements where requirementId = ? and coursePrefix = ? and courseNumber = ?");
+				$this->singlesection = $this->dbconn->prepare("SELECT * from StaticReport where callNumber = ?");
+				$this->listcourses = $this->dbconn->prepare("SELECT * from Requirements where requirementId = ?");
 				$this->listsections = $this->dbconn->prepare("SELECT * from StaticReport where coursePrefix = ? and courseNumber = ?");
 			}
 			$this->errorMessage = "";
-		} catch(PDOException $e) {
+		} catch(Exception $e) {
 			echo 'ERROR: ' . $e->getMessage();
 		}
 	}
 
-	//Retrieve a record from the table
-	//and returns a course object
-	function getCourses($id,$prefix,$number){
+	/**Retrieve a list of records that fulfill a particular requirement
+	 * @param int $id requirement Id
+	 * @return array() of course objects
+	 */
+	function getCourses($id){
 		try{
+			$courseListing = null;
+			$resultset = null;
 			if (!($this->listcourses)){
 				echo "Prepare failed: (" . $this->dbconn->errno . ") " . $this->dbconn->error;
-			}else if (!($this->listcourses->bind_param("iss",$id,$prefix,$number))){
+			}else if (!($this->listcourses->bind_param("i",$id))){
 				echo "Binding parameters failed: (" . $this->listcourses->errno . ") " . $this->listcourses->error;
 			}else if (!($this->listcourses->execute())){
 				echo "Execute failed: (" . $this->listcourses->errno . ") " . $this->listcourses->error;
-			}else if (!($this->listcourses->bind_result($requirementId,$coursePrefix,$courseNumber))){
-				echo "Binding results failed: (" . $this->listcourses->errno . ") " . $this->listcourses->error;
-			}else if (!($this->listcourses->store_result()) && $this->dbconn->errno){
+			}else if (!(($stored = $this->listcourses->store_result())) && $this->dbconn->errno){
 				//switched from using fetch() to store_result() because of mysql error 2014 about commands being out of sync
 				//storeresult buffers the fetched data
 				echo "Fetch failed (DB): (" . $this->dbconn->errno . ") " . $this->dbconn->error;
 				echo "Fetch failed (STMT): (" . $this->listcourses->errno . ") " . $this->listcourses->error;
+			}else if (!($this->listcourses->bind_result($requirementId,$coursePrefix,$courseNumber))){
+				echo "Binding results failed: (" . $this->listcourses->errno . ") " . $this->listcourses->error;
+			}else{
+				if ($stored){
+					$recordcount = $this->listcourses->num_rows;
+					$fieldcount = $this->listcourses->field_count;
+					printf("Number of rows: %d.\n", $recordcount);
+					while($this->listcourses->fetch()){
+						$course = new Course($coursePrefix,$courseNumber);
+						$sections = $this->getSections($coursePrefix,$courseNumber);
+						if ($sections != null){
+							foreach($sections as $section){
+								if (!($course->addSection($section))){
+									echo "Problem adding section to course object - " . $course->getErrorMessage() . "\n";
+								}
+							}
+						}
+						$courseListing[] = $course;
+					}
+				}else{
+					$this->errorMessage = "Error storing results.";
+				}
+			}
+			$this->listcourses->free_result();
+			return $courseListing;
+		}catch(Exception $e){
+			echo $e->getMessage();
+		}
+		$this->listcourses->close();
+		return null;
+	}
+
+	/**Retrieve a record from the table
+	 * e.g. retrieve a single CSCI 1302 course
+	 * @param int $id requirement Id
+	 * @param String $prefix e.g. CSCI
+	 * @param String $number e.g. 1302
+	 * @return Course object
+	 */
+	function getCourse($id,$prefix,$number){
+		try{
+			$course = null;
+			if (!($this->singlecourse)){
+				echo "Prepare failed: (" . $this->dbconn->errno . ") " . $this->dbconn->error;
+			}else if (!($this->singlecourse->bind_param("iss",$id,$prefix,$number))){
+				echo "Binding parameters failed: (" . $this->singlecourse->errno . ") " . $this->singlecourse->error;
+			}else if (!($this->singlecourse->execute())){
+				echo "Execute failed: (" . $this->singlecourse->errno . ") " . $this->singlecourse->error;
+			}else if (!($this->singlecourse->store_result()) && $this->dbconn->errno){
+				//switched from using fetch() to store_result() because of mysql error 2014 about commands being out of sync
+				//storeresult buffers the fetched data
+				echo "Fetch failed (DB): (" . $this->dbconn->errno . ") " . $this->dbconn->error;
+				echo "Fetch failed (STMT): (" . $this->singlecourse->errno . ") " . $this->singlecourse->error;
+			}else if (!($this->singlecourse->bind_result($requirementId,$coursePrefix,$courseNumber))){
+				echo "Binding results failed: (" . $this->singlecourse->errno . ") " . $this->singlecourse->error;
 			}else if ($requirementId){
 				//YAY!
 				$course = new Course($coursePrefix,$courseNumber);
@@ -54,18 +115,19 @@ class DBHelper{
 						}
 					}
 				}
+				return $course;
 			}
-			$this->listcourses->close();
-			return $course;
 		}catch(Exception $e){
 			echo $e->getMessage();
 		}
-		$this->listcourses->close();
 		return null;
 	}
 
-	//Return the sections for the coursePrefix & courseNumber
-	//with the class meetings stuffed inside.
+	/**Return the sections for the coursePrefix & courseNumber
+	 * with the class meetings stuffed inside.
+	 * Returns null if no sections are found.
+	 * @return array() of sections
+	 */
 	function getSections($prefix,$number){
 		try{
 			$sectionListing = array();
@@ -110,13 +172,24 @@ class DBHelper{
 					echo "Fetch failed (STMT): (" . $this->listsections->errno . ") " . $this->listsections->error;
 				}
 			}
-			$this->listsections->close();
 			return $sectionListing;
 		}catch(Exception $e){
 			echo $e->getMessage();
 		}
-		$this->listsections->close();
 		return null;
+	}
+
+	/**
+	 *
+	 *
+	 * @return Section object
+	 */
+	function getSingleSection($callNumber){
+		try{
+
+		}catch(Exception $e){
+			echo $e->getMessage();
+		}
 	}
 
 	/**
--- tests/dbh.php
+++ tests/dbh.php
@@ -6,9 +6,7 @@
 	require_once("../helpers/DBHelper.php");
 	//Testing dbhelper
 	$db = new DBHelper();
-	$course = $db->getCourses(19,"CSCI","2150");
-	$sections = $db->getSections("CSCI","1302");
-	$sections2150 = $db->getSections("CSCI","2150");
+	$course = $db->getCourse(19,"CSCI","2150");
 	if ($course){
 		echo $course->getCoursePrefix() . "-" . $course->getCourseNumber() . "\n";
 		//var_dump($course);
@@ -16,18 +14,40 @@
 		echo "CSCI 2150 not found.\n";
 	}
 
-	if ($sections){
-		echo count($sections) . " found for CSCI.\n";
-		print_r($sections);
-	}else{
-		echo "Sections for CSCI 1302 not found.\n";
-	}
-
+	$sections2150 = $db->getSections("CSCI","2150");
 	if ($sections2150){
 		echo count($sections2150) . " found for CSCI.\n";
 		print_r($sections2150);
 	}else{
 		echo "Sections for CSCI 2150 not found.\n";
 	}
+
+	$courses18 = $db->getCourses(18);
+	if ($courses18){
+		echo "Courses found: " . count($courses18) . "\n";
+		print_r($courses18);
+	}else{
+		echo "Courses not found for requirement 18.\n";
+	}
+
+	$courses19 = $db->getCourses(19);
+	if ($courses19){
+		echo "Courses found: " . count($courses19) . "\n";
+	}else{
+		echo "Courses not found for requirement 19.\n";
+	}
+
+	$sections = $db->getSections("CSCI","1302");
+	if ($sections){
+		echo count($sections) . " found for CSCI.\n";
+	}else{
+		echo "Sections for CSCI 1302 not found.\n";
+	}
+
+
+
+
+
+
 
 ?>
